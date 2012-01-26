c459f5c23dd43784b56db8ee0dbb6a68e65ccc31
neverloved <doctornever@gmail.com>
2012-01-26 11:56:10 +0300

    Little part of future engine' logic.

--- temp/index.php
+++ temp/index.php
@@ -23,19 +23,20 @@ function initscript(block)
 
 	</script>
 <div id="content">
-<form method="post" action="index.html">
+<form method="post" action="functions.php">
 <table><tr><td>
 <table>
 <tr>
-<td><label class="events_anonce">Тип ящика</label></td><td id="boxtype"> <label onclick="initscript('#boxtype')">\получить из базы\</label></td><!--<input type="text" checked name="boxtype" size="30" /></td>-->
+<td><label class="events_anonce">Тип ящика</label></td><td id="networkboxtype"> <label onclick="initscript('#networkboxtype')">\получить из базы\</label></td><!--<input type="text" checked name="boxtype" size="30" /></td>-->
 <br />
 </tr>
 <tr>
-<td><label class="events_anonce">Инв. номер</label></td><td id="invnum"> <label onclick="initscript('#invnum')">\получить из базы\</label></td><!--<td><input type="text" name="invmun" size="30" /></td>-->
+<td><label class="events_anonce">Инв. номер</label></td><td id="inventorynumber"> <label onclick="initscript('#inventorynumber')">\получить из базы\</label></td><!--<td><input type="text" name="invmun" size="30" /></td>-->
 <br />
 </tr>
 <tr>
-<td><label class="events_anonce">deprecated</label></td><td><input type="text" name="" size="30" /></td>
+<td><label class="events_anonce">deprecated</label></td><td><input type="hidden" name="whichadded" value="networkbox" size="30" /></td>
+</tr><tr><td><input type="submit" />
 </table>
 </td></tr>
 <tr><td>
@@ -45,23 +46,24 @@ function initscript(block)
 <br />
 </tr>
 <tr>
-<td><label class="events_anonce">Производитель</label></td><td id="manufacterer"> <label onclick="initscript('#manufacterer')">\получить из базы\</label></td><!--<input type="text" checked name="boxtype" size="30" /></td>--><!--<td><input type="text" name="invmun" size="30" /></td>-->
+<td><label class="events_anonce">Производитель</label></td><td id="manufacturer"> <label onclick="initscript('#manufacturer')">\получить из базы\</label></td><!--<input type="text" checked name="boxtype" size="30" /></td>--><!--<td><input type="text" name="invmun" size="30" /></td>-->
 <br />
 </tr>
 <tr>
-<td><label class="events_anonce">Унитс</label></td><td id="units"> <label onclick="initscript('#units')">\получить из базы\</label></td>
+<td><label class="events_anonce">Units</label></td><td id="units"> <label onclick="initscript('#units')">\получить из базы\</label></td>
 </tr>
 <tr>
-<td><label class="events_anonce">Видтх</label></td><td id="width"> <label onclick="initscript('#width')">\получить из базы\</label></td>
+<td><label class="events_anonce">Width</label></td><td id="width"> <label onclick="initscript('#width')">\получить из базы\</label></td>
 </tr>
 <tr>
-<td><label class="events_anonce">Хеигхт</label></td><td id="marking"> <label onclick="initscript('#height')">\получить из базы\</label></td>
+<td><label class="events_anonce">Height</label></td><td id="height"> <label onclick="initscript('#height')">\получить из базы\</label></td>
 </tr>
 <tr>
-<td><label class="events_anonce">Ленгтх</label></td><td id="length"> <label onclick="initscript('#length')">\получить из базы\</label></td>
+<td><label class="events_anonce">Length</label></td><td id="length"> <label onclick="initscript('#length')">\получить из базы\</label></td>
 </tr>
 <tr>
-<td><label class="events_anonce">Диаметыр</label></td><td id="diameter"> <label onclick="initscript('#diameter')">\получить из базы\</label></td>
+<td><label class="events_anonce">Diameter</label></td><td id="diameter"> <label onclick="initscript('#diameter')">\получить из базы\</label></td>
+</tr><td><input type="hidden" name="whichadded" value="networkboxtype" /></td><td><input type="submit" /></td>
 </tr>
 
 </table>
