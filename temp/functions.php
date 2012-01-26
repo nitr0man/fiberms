13d22b4f742f9892d66cc54da078dd8524235c34
adidas <xkadidas@gmail.com>
2012-01-26 12:34:25 +0200

    2

--- temp/functions.php
+++ temp/functions.php
@@ -16,15 +16,15 @@ if($_SERVER["REQUEST_METHOD"] == 'POST')
         	    marking, manufacturer, units, width, height, length, diameter)
 		    	VALUES ('$_POST[#marking]', '$_POST[#manufacturer]', '$_POST[#units]', '$_POST[#width]', '$_POST[#height]', '$_POST[#length]', '$_POST[#diameter]');");*/
 		    	$marking = $_POST['#marking'];
-		    	$manufacturer = $_POST['#$manufacturer'];
-		    	$units = $_POST['#$units'];
-		    	$width = $_POST['#$width'];
-		    	$height = $_POST['#$height'];
-		    	$length = $_POST['#$length'];
-		    	$diameter = $_POST['#$diameter'];
+		    	$manufacturer = $_POST['#manufacturer'];
+		    	$units = $_POST['#units'];
+		    	$width = $_POST['#width'];
+		    	$height = $_POST['#height'];
+		    	$length = $_POST['#length'];
+		    	$diameter = $_POST['#diameter'];
 		    	PQuery("INSERT INTO NetworkBoxType(
         	    marking, manufacturer, units, width, height, length, diameter)
-		    	VALUES ('$marking', '$manufacturer', '$units', '$width', '$height', '$length', '$diameter');");
+		    	VALUES ('$marking', '$manufacturer', '$units', '$width', '$height', '$length', '$diameter')");
 		    	print("done");
 			}
 		}
