2ae774ebe73e03726aab32dc00bbe707e02e4a6a
neverloved <doctornever@gmail.com>
2012-01-26 10:50:49 +0300

    index.php has been modified

--- temp/index.php
+++ temp/index.php
@@ -5,7 +5,7 @@
  <div class="menu">
  <h3>Menu header</h3>
  <ul>
-	<li id="li4">element1</li>
+	<li id="li4">NetworkBox</li>
 	<li id="li4">element2</li>
 	<li id="li4">element3</li>
 	<li id="li4">element4</li>
@@ -21,6 +21,7 @@ function initscript() {
 	</script>
 <div id="content">
 <form method="post" action="index.html">
+<table><tr><td>
 <table>
 <tr>
 <td><label class="events_anonce">Тип ящика</label></td><td id="boxtype"> <label onclick="initscript()">\получить из базы\</label></td><!--<input type="text" checked name="boxtype" size="30" /></td>-->
@@ -32,7 +33,35 @@ function initscript() {
 </tr>
 <tr>
 <td><label class="events_anonce">deprecated</label></td><td><input type="text" name="" size="30" /></td>
+</table>
+</td></tr>
+<tr><td>
+<table>
+<tr>
+<td><label class="events_anonce">Маркировка</label></td><td id="boxtype"> <label onclick="initscript()">\получить из базы\</label></td><!--<input type="text" checked name="boxtype" size="30" /></td>-->
+<br />
+</tr>
+<tr>
+<td><label class="events_anonce">Производитель</label></td><td><input type="text" name="invmun" size="30" /></td>
+<br />
+</tr>
+<tr>
+<td><label class="events_anonce">Унитс</label></td><td><input type="text" name="" size="30" /></td>
+</tr>
+<tr>
+<td><label class="events_anonce">Видтх</label></td><td><input type="text" name="invmun" size="30" /></td>
+</tr>
+<tr>
+<td><label class="events_anonce">Хеигхт</label></td><td><input type="text" name="invmun" size="30" /></td>
+</tr>
+<tr>
+<td><label class="events_anonce">Ленгтх</label></td><td><input type="text" name="invmun" size="30" /></td>
+</tr>
+<tr>
+<td><label class="events_anonce">Диаметыр</label></td><td><input type="text" name="invmun" size="30" /></td>
+</tr>
 
+</table>
 <br />
 
 </form>
