698bef8d9c556e83ca4764d7d95d3f4d73467e22
adidas <xkadidas@gmail.com>
2012-01-26 12:00:21 +0200

    functions.php +config.php! Are u ready? :D

---
+++ temp/config.php
@@ -0,0 +1,8 @@
+﻿<?php
+$host = "nitr0.homelinux.net";
+$user = "development";
+$pass = "devpass12";
+$db = "dev";
+$connection = pg_connect ("host=myhost port=5432 dbname=mydb
+user=postgres password-postgres");
+?>
---
+++ temp/functions.php
@@ -0,0 +1,20 @@
+﻿<?php
+require_once("config.php");
+
+function PQuery($query)
+{	pg_query($connection, $query) or die(pg_last_error($connection));
+	$qcount++;
+}
+
+
+if($_SERVER["REQUEST_METHOD"] == 'POST')
+	{		function AddNetworkBox;
+		{		if ($_POST['whichadded'] == 'networkboxtype')
+			{    	    	PQuery("INSERT INTO NetworkBoxType(
+        	    marking, manufacturer, units, width, height, length, diameter)
+		    	VALUES ('$_POST[marking]', '$_POST[manufacturer]', '$_POST[units]', '$_POST[width]', '$_POST[height]', '$_POST[length]', '$_POST[diameter]');");
+			}
+		}
+	}
+else print("Брысь!");
+?>
