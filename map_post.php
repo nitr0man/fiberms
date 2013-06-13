6f442e7913e893b5a0112c8d2f630027ad6799ab
dredei <xkadidas@gmail.com>
2013-06-13 13:27:23 +0300

    format

--- map_post.php
+++ map_post.php
@@ -1,14 +1,15 @@
 <?php
-	require_once( "backend/functions.php" );
-	require_once( "backend/map.php" );
-	
-	if ($_SERVER["REQUEST_METHOD"] == 'POST')
-	{
-		$obj = json_decode( $_POST['coors'] );
-		$coors = $obj->{'coorArr'};
-		$CableLine = $obj->{'CableLineId'};
-		$seqStart = $obj->{'seqStart'};
-		$seqEnd = $obj->{'seqEnd'};
-		updCableLinePoints( $coors, $CableLine, $seqStart, $seqEnd );		
-	}
+
+require_once( "backend/functions.php" );
+require_once( "backend/map.php" );
+
+if ( $_SERVER[ "REQUEST_METHOD" ] == 'POST' )
+{
+    $obj = json_decode( $_POST[ 'coors' ] );
+    $coors = $obj->{'coorArr'};
+    $CableLine = $obj->{'CableLineId'};
+    $seqStart = $obj->{'seqStart'};
+    $seqEnd = $obj->{'seqEnd'};
+    updCableLinePoints( $coors, $CableLine, $seqStart, $seqEnd );
+}
 ?>
