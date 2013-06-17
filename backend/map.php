2b8b26ecfc45870fe75309d71e1b0774a957e47f
adidas <xkadidas@gmail.com>
2013-06-17 15:55:21 +0300

    fix

--- backend/map.php
+++ backend/map.php
@@ -4,7 +4,34 @@ function updCableLinePoints( $coors, $CableLine, $seqStart, $seqEnd )
 {
     $query = 'SELECT * FROM "CableLinePoint" WHERE "CableLine" = '.$CableLine.' ORDER BY "sequence"';
     $res = PQuery( $query );
-    if ( $res[ 'rows' ][ $seqEnd ][ 'meterSign' ] != "" )
+    if ( $res[ 'rows' ][ $seqStart ][ 'meterSign' ] != "" && $res[ 'rows' ][ $seqEnd ][ 'meterSign' ] != "" )
+    {
+        $query = 'DELETE FROM "CableLinePoint" WHERE "CableLine" = '.$CableLine.' AND "sequence" > '.$seqStart.' AND "sequence" < '.$seqEnd;
+        PQuery( $query );
+        if ( count( $coors ) != $res[ 'count' ] )
+        {
+            $seqDiff = count( $coors ) - ( $seqEnd - $seqStart );
+            $query = 'UPDATE "CableLinePoint" SET "sequence" = ("sequence" + '.$seqDiff.')*-1 WHERE "CableLine" = '.$CableLine.' AND "sequence" >= '.$seqEnd;
+            PQuery( $query );
+            $query = 'UPDATE "CableLinePoint" SET "sequence" = "sequence" * -1 WHERE "CableLine" = '.$CableLine.' AND "sequence" < 0';
+            PQuery( $query );
+            $seq = $seqStart;
+        }
+        else
+        {
+            $seq = 0;
+        }
+        for ( $i = 0; $i < count( $coors ); $i++ )
+        {
+            $coor = "(".$coors[ $i ]->lon.",".$coors[ $i ]->lat.")";
+            $ins[ 'OpenGIS' ] = $coor;
+            $ins[ 'sequence' ] = $seq++;
+            $ins[ 'CableLine' ] = $CableLine;
+            $query = 'INSERT INTO "CableLinePoint"'.genInsert( $ins );
+            PQuery( $query );
+        }
+    }
+    elseif ( $res[ 'rows' ][ $seqEnd ][ 'meterSign' ] != "" )
     {
         $query = 'DELETE FROM "CableLinePoint" WHERE "CableLine" = '.$CableLine.' AND "sequence" >= '.$seqStart.' AND "sequence" < '.$seqEnd;
         PQuery( $query );
@@ -13,7 +40,6 @@ function updCableLinePoints( $coors, $CableLine, $seqStart, $seqEnd )
             $seqDiff = count( $coors ) - ( $seqEnd - $seqStart );
             $query = 'UPDATE "CableLinePoint" SET "sequence" = ("sequence" + '.$seqDiff.')*-1 WHERE "CableLine" = '.$CableLine.' AND "sequence" >= '.$seqEnd;
             PQuery( $query );
-            //$query = 'UPDATE "CableLinePoint" SET "sequence" = "sequence" * -1 WHERE "CableLine" = '.$CableLine.' AND "sequence" >= '.$seqEnd;
             $query = 'UPDATE "CableLinePoint" SET "sequence" = "sequence" * -1 WHERE "CableLine" = '.$CableLine.' AND "sequence" < 0';
             PQuery( $query );
             $seq = $seqStart;
@@ -34,12 +60,6 @@ function updCableLinePoints( $coors, $CableLine, $seqStart, $seqEnd )
     }
     else if ( $res[ 'rows' ][ $seqStart ][ 'meterSign' ] != "" )
     {
-        /* $seqDiff = count( $coors ) - ( $seqEnd - $seqStart );
-          $query = 'UPDATE "CableLinePoint" SET "sequence" = "sequence" + '.$seqDiff.' WHERE "CableLine" = '.$CableLine;
-          PQuery( $query );
-          $query = 'DELETE FROM "CableLinePoint" WHERE "CableLine" = '.$CableLine.' AND "sequence" > '.$seqStart.' AND "sequence" <= '.$seqEnd;
-          PQuery( $query );
-          $seq = $seqStart; */
         $query = 'DELETE FROM "CableLinePoint" WHERE "CableLine" = '.$CableLine.' AND "sequence" > '.$seqStart.' AND "sequence" <= '.$seqEnd;
         PQuery( $query );
         if ( count( $coors ) != $res[ 'count' ] )
@@ -47,7 +67,6 @@ function updCableLinePoints( $coors, $CableLine, $seqStart, $seqEnd )
             $seqDiff = count( $coors ) - ( $seqEnd - $seqStart );
             $query = 'UPDATE "CableLinePoint" SET "sequence" = ("sequence" + '.$seqDiff.')*-1 WHERE "CableLine" = '.$CableLine.' AND "sequence" > '.$seqEnd;
             PQuery( $query );
-            //$query = 'UPDATE "CableLinePoint" SET "sequence" = "sequence" * -1 WHERE "CableLine" = '.$CableLine.' AND "sequence" > '.$seqEnd;
             $query = 'UPDATE "CableLinePoint" SET "sequence" = "sequence" * -1 WHERE "CableLine" = '.$CableLine.' AND "sequence" < 0';
             PQuery( $query );
             $seq = $seqStart + 1;
