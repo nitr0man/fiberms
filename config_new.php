fc17466ec45ea83c282f765567dcd17f96b2e504
adidas <xkadidas@gmail.com>
2014-02-19 22:43:30 +0200

    format

--- config_new.php
+++ config_new.php
@@ -1,16 +1,18 @@
 <?php
+
 require_once("backend/functions.php");
 
-$config['host'] = "localhost";
-$config['user'] = "postgres";
-$config['pass'] = "";
-$config['db'] = "dev";
-$config['LinesPerPage'] = 10;
-$config['version'] = '1.0.0';
+$config[ 'host' ] = "localhost";
+$config[ 'user' ] = "postgres";
+$config[ 'pass' ] = "";
+$config[ 'db' ] = "dev";
+$config[ 'LinesPerPage' ] = 10;
+$config[ 'version' ] = '1.0.0';
 $query_count = 0;
-$connection = PConnect($config['host'], $config['db'], $config['user'], $config['pass']);
-if (!$connection) {
-	die("Could not open connection to database server");
+$connection = PConnect( $config[ 'host' ], $config[ 'db' ], $config[ 'user' ],
+     $config[ 'pass' ] );
+if ( !$connection )
+{
+    die( "Could not open connection to database server" );
 }
-
 ?>
